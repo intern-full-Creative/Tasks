b95398a1c576a6a4a9abe2f986dd835991482749
Jith <jith4j@gmail.com>
2021-08-19 19:11:20 +0530

    added delete button and made items stack on top

--- to-do/app.js
+++ to-do/app.js
@@ -2,16 +2,28 @@ let todoInput = document.getElementById("todo-input");
 let todoBtn = document.getElementById("todo-btn");
 let todoContainer = document.getElementById("todo-container");
 
-todoBtn.addEventListener("click", () => {
-  var paragraph = document.createElement("p");
+let addTodo = () => {
+  let todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+
+  const paragraph = document.createElement("span");
   paragraph.classList.add("paragraph");
   paragraph.innerHTML = todoInput.value;
-  todoContainer.appendChild(paragraph);
+
+  todoDiv.appendChild(paragraph);
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "X";
+  deleteButton.classList.add("delete-btn");
+  todoDiv.appendChild(deleteButton);
+
+  todoContainer.prepend(todoDiv);
   todoInput.value = "";
   paragraph.addEventListener("click", () => {
     paragraph.style.textDecoration = "line-through";
   });
-  paragraph.addEventListener("dblclick", () => {
-    todoContainer.removeChild(paragraph);
+  deleteButton.addEventListener("click", () => {
+    todoContainer.removeChild(todoDiv);
   });
-});
+};
+
+todoBtn.addEventListener("click", addTodo);
