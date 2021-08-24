0e1ceb4846a6472ac17adabd66e38dfd0b7d21d0
Jith <jith4j@gmail.com>
2021-08-24 15:42:38 +0530

    promise task updated

--- promise-task/app.js
+++ promise-task/app.js
@@ -1,7 +1,13 @@
+let completedPromise = [];
+let incompletedPromise = [];
+
 let createPromis = (time, n) => {
   return new Promise((resolve, reject) => {
+    if (time < 5000) {
+      resolve(n);
+    }
     setTimeout(() => {
-      resolve(`Completed p${n}`);
+      reject(n);
     }, time);
   });
 };
@@ -14,13 +20,15 @@ let p5 = createPromis(8000, 5);
 let p6 = createPromis(9000, 6);
 let p7 = createPromis(3000, 7);
 
-let promises = [p1, p2, p3, p4, p5, p6, p7];
-
 setTimeout(() => {
-  promises.forEach(checkPromise);
+  console.log(`The completed promises are: ${completedPromise}`);
 }, 5000);
 
-let checkPromise = (p) => {
-  let result = p.then(console.log(p)).catch();
-  return result;
-};
+let promises = [p1, p2, p3, p4, p5, p6, p7];
+
+promises.forEach((element) => {
+  element.then(
+    (n) => completedPromise.push(n),
+    (n) => console.log(`Promise ${n} is incomplete`)
+  );
+});
