36a3608a162fc81aa3d029837f18f62cc4c99ea5
Jith <jith4j@gmail.com>
2021-08-26 11:40:30 +0530

    updated some code

--- callback/app.js
+++ callback/app.js
@@ -1,7 +1,3 @@
-window.onload = function () {
-  document
-    .querySelector("#callback-btn")
-    .addEventListener("click", function () {
-      console.log("User has clicked on the button!");
-    });
-};
+document.querySelector("#callback-btn").addEventListener("click", function () {
+  console.log("User has clicked on the button!");
+});
